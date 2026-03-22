bcbbafb4033fc67327e0c04833eecc265e4116de
Libra <skorohodignat59@gmail.com>
2026-03-22 07:13:20 +0200

    Final update

--- routes/catalog.js
+++ routes/catalog.js
@@ -9,36 +9,52 @@ const bookinstance_controller = require("../controllers/bookinstanceController")
 // HOME
 router.get("/", book_controller.index);
 
-// AUTHOR CREATE
+//
+// AUTHOR
+//
 router.get("/author/create", author_controller.author_create_get);
 router.post("/author/create", author_controller.author_create_post);
 
-// GENRE CREATE
-router.get("/genre/create", genre_controller.genre_create_get);
-router.post("/genre/create", genre_controller.genre_create_post);
+router.get("/author/:id/delete", author_controller.author_delete_get);
+router.post("/author/:id/delete", author_controller.author_delete_post);
 
-// BOOK CREATE
-router.get("/book/create", book_controller.book_create_get);
-router.post("/book/create", book_controller.book_create_post);
-
-// BOOKINSTANCE CREATE
-router.get("/bookinstance/create", bookinstance_controller.bookinstance_create_get);
-router.post("/bookinstance/create", bookinstance_controller.bookinstance_create_post);
-
-// BOOKS
-router.get("/books", book_controller.book_list);
-router.get("/book/:id", book_controller.book_detail);
-
-// BOOK INSTANCES
-router.get("/bookinstances", bookinstance_controller.bookinstance_list);
-router.get("/bookinstance/:id", bookinstance_controller.bookinstance_detail);
-
-// AUTHORS
 router.get("/authors", author_controller.author_list);
 router.get("/author/:id", author_controller.author_detail);
 
-// GENRES
+//
+// GENRE
+//
+router.get("/genre/create", genre_controller.genre_create_get);
+router.post("/genre/create", genre_controller.genre_create_post);
+
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+
 router.get("/genres", genre_controller.genre_list);
 router.get("/genre/:id", genre_controller.genre_detail);
+
+//
+// BOOK
+//
+router.get("/book/create", book_controller.book_create_get);
+router.post("/book/create", book_controller.book_create_post);
+
+router.get("/book/:id/delete", book_controller.book_delete_get);
+router.post("/book/:id/delete", book_controller.book_delete_post);
+
+router.get("/books", book_controller.book_list);
+router.get("/book/:id", book_controller.book_detail);
+
+//
+// BOOKINSTANCE
+//
+router.get("/bookinstance/create", bookinstance_controller.bookinstance_create_get);
+router.post("/bookinstance/create", bookinstance_controller.bookinstance_create_post);
+
+router.get("/bookinstance/:id/delete", bookinstance_controller.bookinstance_delete_get);
+router.post("/bookinstance/:id/delete", bookinstance_controller.bookinstance_delete_post);
+
+router.get("/bookinstances", bookinstance_controller.bookinstance_list);
+router.get("/bookinstance/:id", bookinstance_controller.bookinstance_detail);
 
 module.exports = router;
